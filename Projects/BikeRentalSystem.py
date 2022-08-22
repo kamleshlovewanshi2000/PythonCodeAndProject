152e3e77713e1d1b79e9fa03f51fdd5c7d3bb79d
KAMLESH LOVEWANSHI <kamleshlovewanshi2000@gmail.com>
2022-08-22 18:18:15 +0530

    initial commit

---
+++ Projects/BikeRentalSystem.py
@@ -0,0 +1,38 @@
+"""
+1. Display Available Bikes
+2. Request a Bike for Rent(100 INR --> 1 Qty)
+3. exit
+"""
+class BickShop:
+
+    def __init__(self,stock):
+        self.stock = stock
+
+    def displayAvailableBike(self):
+        print("Total available Bike:",self.stock)
+
+    def rentForBike(self,q):
+        if q<=0:
+            print("Enter the positive Number or Greater then zero(0):")
+        elif q>self.stock:
+            print("Enter the Value (Less then Stock):")
+        else:
+            self.stock = self.stock-q
+            print("Total Prices for Bike Rent:", q*100)
+            print("Total Bikes Available in stock:",self.stock)
+
+while True:
+    obj = BickShop(100)
+    uc = int(input("""
+    1. Display Available Bikes
+    2. Request a Bike for Rent(100 INR --> 1 Qty)
+    3. exit
+    :-"""))
+    if uc == 1:
+        obj.displayAvailableBike()
+    elif uc == 2:
+        n = int(input("Enter the Quantity of Bike:---"))
+        obj.rentForBike(n)
+    else:
+        print("Your Exit!....")
+        break
